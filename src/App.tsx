99e0ebd192e3158f508ea1279b0a86fbc606772b
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-07 12:02:46 +0100

    Connect / disconnect on button click testing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 import { testcall } from "./services/testCalls";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 function App() {
+  const [connectState, setConnectState] = useState<boolean>();
+  const [socket, setSocket] = useState<Socket | null>();
   const [count, setCount] = useState(0);
-  const socket = io("http://localhost:3000/");
 
   const handleClick = async () => {
-    socket.on("click", (msg: "clicked") => {});
+    if (!connectState) {
+      const newSocket = io("http://localhost:3000/");
+      console.log(newSocket);
+      setSocket(newSocket);
+      newSocket.on("connect", () => {
+        console.log("connected to socket");
+        setConnectState(true);
+      });
+
+      // test sending a message
+      newSocket.emit("chat message", "hello world");
+    } else {
+      if (socket) {
+        socket.disconnect();
+        console.log("disconnected user");
+        setConnectState(false);
+      }
+    }
   };
 
   return (
