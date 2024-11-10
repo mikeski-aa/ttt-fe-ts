34794d2a1dc863467a46c141d0f39a2471644860
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-10 17:58:59 +0100

    Working on login validation

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -14,6 +14,11 @@ function LoginModal({
   const [regConfPw, setRegConfPw] = useState<string>("");
   const [logUname, setLogUname] = useState<string>("");
   const [logPw, setLogPw] = useState<string>("");
+  const [regUError, setRegUError] = useState<boolean>(false);
+  const [regPError, setRegPError] = useState<boolean>(false);
+  const [regCError, setRegCError] = useState<boolean>(false);
+  const [errText, setErrText] = useState<string>("");
+  const [errPwText, setErrPwText] = useState<string>("");
 
   const handleModalClose = () => {
     setModalState(!modalState);
@@ -31,21 +36,54 @@ function LoginModal({
   const handleRegisterClick = async (e: SyntheticEvent) => {
     console.log("reg clicked");
     e.preventDefault();
+
+    if (regUname.length < 1) {
+      setRegUError(true);
+      setErrText("Username is required");
+    } else {
+      setRegUError(false);
+    }
+
+    if (regPw.length < 5) {
+      setRegPError(true);
+      setErrPwText("Password is too short");
+    } else {
+      setRegPError(false);
+      setErrPwText("");
+    }
+
+    if (regConfPw.length < 5) {
+      setRegCError(true);
+      setErrPwText("Password is too short");
+    } else {
+      setRegCError(false);
+      setErrPwText("");
+    }
   };
 
   // set up all input handlers
   const handleInput = (input: string, e: SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
-    if (input === "regU") {
-      setRegUname(target.value);
-    } else if (input === "regP") {
-      setRegPw(target.value);
-    } else if (input === "regC") {
-      setRegConfPw(target.value);
-    } else if (input === "logU") {
-      setLogUname(target.value);
-    } else if (input === "logP") {
-      setLogPw(target.value);
+
+    switch (input) {
+      case "regU":
+        if (target.value.length > 0) {
+          setRegUError(false);
+        }
+        setRegUname(target.value);
+        break;
+      case "regP":
+        setRegPw(target.value);
+        break;
+      case "regC":
+        setRegConfPw(target.value);
+        break;
+      case "logU":
+        setLogUname(target.value);
+        break;
+      case "logP":
+        setLogPw(target.value);
+        break;
     }
   };
 
@@ -66,6 +104,8 @@ function LoginModal({
                 placeholder="username"
                 className="formInput"
                 value={logUname}
+                min={1}
+                max={16}
                 onChange={(e) => handleInput("logU", e)}
               ></input>
               <input
@@ -90,27 +130,50 @@ function LoginModal({
         ) : (
           <div className={`registerDiv`}>
             <form className="logregform">
-              <input
-                type="text"
-                placeholder="username"
-                className="formInput"
-                value={regUname}
-                onChange={(e) => handleInput("regP", e)}
-              ></input>
-              <input
-                type="password"
-                placeholder="password"
-                className="formInput"
-                value={regPw}
-                onChange={(e) => handleInput("regU", e)}
-              ></input>
+              <div className="inputHolder">
+                <input
+                  type="text"
+                  placeholder="username"
+                  className="formInput"
+                  value={regUname}
+                  min={1}
+                  max={16}
+                  onChange={(e) => handleInput("regU", e)}
+                ></input>
+                <div className={regUError ? "error show" : "error hide"}>
+                  {errText}
+                </div>
+              </div>
+
+              <div className="inputHolder">
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="formInput"
+                  value={regPw}
+                  onChange={(e) => handleInput("regP", e)}
+                  min={4}
+                  max={16}
+                ></input>
+                <div className={regPError ? "error show" : "error hide"}>
+                  {errPwText}
+                </div>
+              </div>
               <input
                 type="password"
                 placeholder="confirm password"
                 className="formInput"
                 value={regConfPw}
+                min={4}
+                max={16}
                 onChange={(e) => handleInput("regC", e)}
               ></input>
+
+              <div className="inputHolder">
+                <div className={regConfPw ? "error show" : "error hide"}>
+                  {errPwText}
+                </div>
+              </div>
 
               <button
                 type="submit"
